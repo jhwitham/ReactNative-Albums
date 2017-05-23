ef950795469ff8312eb18eeae7875c02ae405217
John Herman Witham <john.h.witham@gmail.com>
2017-05-24 01:23:52 +0300

    Created barebones for App component

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,13 @@
 // Import library to help create a Component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
+
 // Create a Component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
 
 // Render it to the device
+AppRegistry.registerComponent('albums', () => App);
